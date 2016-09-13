31f9806ca99a76fe73fe4fe08d4dfcc2cb7a849e
iamxy <kenneth.liu.xy@gmail.com>
2016-09-13 14:40:58 +0800

    *: some clean up

--- proto/generate.sh
+++ proto/generate.sh
@@ -8,6 +8,6 @@ for FILE in pump; do
 protoc -I.:${GOGO_ROOT}:${GOGO_ROOT}/protobuf --gofast_out=plugins=grpc:. proto/${FILE}.proto
 sed -i.bak -E 's/import _ \"gogoproto\"//g' proto/${FILE}.pb.go
 sed -i.bak -E 's/import fmt \"fmt\"//g' proto/${FILE}.pb.go
-goimports -w proto/${FILE}.pb.go
 rm -f proto/${FILE}.pb.go.bak
+goimports -w proto/${FILE}.pb.go
 done
