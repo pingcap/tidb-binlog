654c31dbc33281345c419eb875e1c9d6cab6fbc8
iamxy <kenneth.liu.xy@gmail.com>
2016-09-26 13:49:46 +0800

    proto: add binlog-server gRPC interfaces.

--- proto/generate.sh
+++ proto/generate.sh
@@ -4,11 +4,25 @@ echo "generate go code..."
 
 GOGO_ROOT=${GOPATH}/src/github.com/gogo/protobuf
 
-for FULLNAME in `ls proto/*.proto`; do
-FILE=`basename ${FULLNAME%.*}`
-protoc -I.:${GOGO_ROOT}:${GOGO_ROOT}/protobuf --gofast_out=plugins=grpc:. proto/${FILE}.proto
-sed -i.bak -E 's/import _ \"gogoproto\"//g' proto/${FILE}.pb.go
-sed -i.bak -E 's/import fmt \"fmt\"//g' proto/${FILE}.pb.go
-rm -f proto/${FILE}.pb.go.bak
-goimports -w proto/${FILE}.pb.go
-done
+function gen()
+{
+    protoc -I.:${GOGO_ROOT}:${GOGO_ROOT}/protobuf --gofast_out=plugins=grpc:. proto/$1.proto
+    sed -i.bak -E 's/import _ \"gogoproto\"//g' proto/$1.pb.go
+    sed -i.bak -E 's/import fmt \"fmt\"//g' proto/$1.pb.go
+    rm -f proto/$1.pb.go.bak
+    goimports -w proto/$1.pb.go
+}
+
+if [ $# -eq 0 ]; then
+    for FULLNAME in `ls proto/*.proto`; do
+        FILE=`basename ${FULLNAME%.*}`
+        gen $FILE
+    done
+else
+    until [ $# -eq 0 ]
+    do
+    gen $1
+    shift
+    done
+fi
+
