fdae56627b807882793327551754e2acc880f76a
iamxy <kenneth.liu.xy@gmail.com>
2016-10-25 17:59:51 +0800

    cistern: fix the bug of dump ddl jobs at init.

--- cistern/server.go
+++ cistern/server.go
@@ -139,28 +139,28 @@ func (s *Server) DumpBinlog(req *binlog.DumpBinlogReq, stream binlog.Cistern_Dum
 			},
 		)
 		if err != nil {
-			log.Errorf("gRPC: DumpBinlog scan boltdb error, %v", err)
+			log.Errorf("gRPC: DumpBinlog scan boltdb error, %s", errors.ErrorStack(err))
 			return errors.Trace(err)
 		}
 
 		for _, resp := range resps {
 			item := &binlog.Binlog{}
 			if err := item.Unmarshal(resp.Payload); err != nil {
-				log.Errorf("gRPC: DumpBinlog unmarshal binlog error, %v", err)
+				log.Errorf("gRPC: DumpBinlog unmarshal binlog error, %s", errors.ErrorStack(err))
 				return errors.Trace(err)
 			}
 			if item.DdlJobId > 0 {
 				key := codec.EncodeInt([]byte{}, item.DdlJobId)
 				data, err := s.boltdb.Get(ddlJobNamespace, key)
 				if err != nil {
-					log.Errorf("DDL Job(%d) not found, with binlog commitTS(%d), %v", item.DdlJobId, resp.CommitTS, err)
+					log.Errorf("DDL Job(%d) not found, with binlog commitTS(%d), %s", item.DdlJobId, resp.CommitTS, errors.ErrorStack(err))
 					return errors.Annotatef(err,
 						"DDL Job(%d) not found, with binlog commitTS(%d)", item.DdlJobId, resp.CommitTS)
 				}
 				resp.Ddljob = data
 			}
 			if err := stream.Send(resp); err != nil {
-				log.Errorf("gRPC: DumpBinlog send stream error, %v", err)
+				log.Errorf("gRPC: DumpBinlog send stream error, %s", errors.ErrorStack(err))
 				return errors.Trace(err)
 			}
 			latest = resp.CommitTS
@@ -186,7 +186,7 @@ func (s *Server) DumpDDLJobs(ctx context.Context, req *binlog.DumpDDLJobsReq) (*
 			if err != nil {
 				return false, errors.Trace(err)
 			}
-			if cts > upperTS {
+			if cts > upperTS && lastTS > 0 {
 				return false, nil
 			}
 			payload, _, err := decodePayload(val)
@@ -205,7 +205,7 @@ func (s *Server) DumpDDLJobs(ctx context.Context, req *binlog.DumpDDLJobsReq) (*
 		},
 	)
 	if err != nil {
-		log.Errorf("gRPC: DumpDDLJobs scan boltdb error, %v", err)
+		log.Errorf("gRPC: DumpDDLJobs scan boltdb error, %v", errors.ErrorStack(err))
 		return nil, errors.Trace(err)
 	}
 
@@ -217,10 +217,7 @@ func (s *Server) DumpDDLJobs(ctx context.Context, req *binlog.DumpDDLJobsReq) (*
 		return s.getAllHistoryDDLJobsByTS(lastTS)
 	}
 
-	resp := &binlog.DumpDDLJobsResp{
-		Ddljobs: [][]byte{},
-	}
-	return resp, nil
+	return nil, errors.Errorf("can't determine the schema version by incoming TS, because there is not any binlog yet.")
 }
 
 func (s *Server) getAllHistoryDDLJobsByID(upperJobID int64) (*binlog.DumpDDLJobsResp, error) {
@@ -241,7 +238,7 @@ func (s *Server) getAllHistoryDDLJobsByID(upperJobID int64) (*binlog.DumpDDLJobs
 		},
 	)
 	if err != nil {
-		log.Errorf("gRPC: DumpDDLJobs getAllHistoryDDLJobsByID error, %v", err)
+		log.Errorf("gRPC: DumpDDLJobs getAllHistoryDDLJobsByID error, %v", errors.ErrorStack(err))
 		return nil, errors.Trace(err)
 	}
 
@@ -254,18 +251,18 @@ func (s *Server) getAllHistoryDDLJobsByID(upperJobID int64) (*binlog.DumpDDLJobs
 func (s *Server) getAllHistoryDDLJobsByTS(ts int64) (*binlog.DumpDDLJobsResp, error) {
 	val, err := s.boltdb.Get(binlogNamespace, codec.EncodeInt([]byte{}, ts))
 	if err != nil {
-		log.Errorf("gRPC: DumpDDLJobs getAllHistoryDDLJobsByTS get boltdb error, %v", err)
+		log.Errorf("gRPC: DumpDDLJobs getAllHistoryDDLJobsByTS get boltdb error, %v", errors.ErrorStack(err))
 		return nil, errors.Trace(err)
 	}
 	payload, _, err := decodePayload(val)
 	if err != nil {
-		log.Errorf("gRPC: DumpDDLJobs getAllHistoryDDLJobsByTS decode payload error, %v", err)
+		log.Errorf("gRPC: DumpDDLJobs getAllHistoryDDLJobsByTS decode payload error, %v", errors.ErrorStack(err))
 		return nil, errors.Trace(err)
 	}
 	item := &binlog.Binlog{}
 	err = item.Unmarshal(payload)
 	if err != nil {
-		log.Errorf("gRPC: DumpDDLJobs getAllHistoryDDLJobsByTS unmarshal payload error, %v", err)
+		log.Errorf("gRPC: DumpDDLJobs getAllHistoryDDLJobsByTS unmarshal payload error, %v", errors.ErrorStack(err))
 		return nil, errors.Trace(err)
 	}
 	if item.Tp != binlog.BinlogType_Commit {
@@ -275,7 +272,7 @@ func (s *Server) getAllHistoryDDLJobsByTS(ts int64) (*binlog.DumpDDLJobsResp, er
 	prewriteValue := &binlog.PrewriteValue{}
 	err = prewriteValue.Unmarshal(item.PrewriteValue)
 	if err != nil {
-		log.Errorf("gRPC: DumpDDLJobs getAllHistoryDDLJobsByTS unmarshal prewriteValue error, %v", err)
+		log.Errorf("gRPC: DumpDDLJobs getAllHistoryDDLJobsByTS unmarshal prewriteValue error, %v", errors.ErrorStack(err))
 		return nil, errors.Trace(err)
 	}
 	upperSchemaVer := prewriteValue.SchemaVersion
@@ -301,7 +298,7 @@ func (s *Server) getAllHistoryDDLJobsByTS(ts int64) (*binlog.DumpDDLJobsResp, er
 		},
 	)
 	if err != nil {
-		log.Errorf("gRPC: DumpDDLJobs getAllHistoryDDLJobsByTS scan boltdb error, %v", err)
+		log.Errorf("gRPC: DumpDDLJobs getAllHistoryDDLJobsByTS scan boltdb error, %v", errors.ErrorStack(err))
 		return nil, errors.Trace(err)
 	}
 
