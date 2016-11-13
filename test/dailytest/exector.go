7db107d2e6593118922bc06b9a1f64ab13964327
argregoryian@gmail.com <GregoryIan@users.noreply.github.com>
2016-11-13 13:59:51 +0800

    test: improve daily test * add truncate table * add drop test table

--- test/dailytest/exector.go
+++ test/dailytest/exector.go
@@ -1,14 +1,15 @@
 package dailytest
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/ngaut/log"
 	"github.com/pingcap/tidb-binlog/test/util"
 )
 
-// Run execute the test case
-func Run(dbCfg util.DBConfig, tableSQLs []string, workerCount int, jobCount int, batch int) {
+// RunDailyTest generates insert/update/delete sqls and execute
+func RunDailyTest(dbCfg util.DBConfig, tableSQLs []string, workerCount int, jobCount int, batch int) {
 	var wg sync.WaitGroup
 	wg.Add(len(tableSQLs))
 
@@ -38,4 +39,48 @@ func Run(dbCfg util.DBConfig, tableSQLs []string, workerCount int, jobCount int,
 	}
 
 	wg.Wait()
+}
+
+// TruncateTestTable truncates test data
+func TruncateTestTable(dbCfg util.DBConfig, tableSQLs []string) {
+	db, err := util.CreateDB(dbCfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer util.CloseDB(db)
+
+	for i := range tableSQLs {
+		table := newTable()
+		err := parseTableSQL(table, tableSQLs[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = execSQL(db, fmt.Sprintf("truncate table %s", table.name))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+// DropTestTable drops test table
+func DropTestTable(dbCfg util.DBConfig, tableSQLs []string) {
+	db, err := util.CreateDB(dbCfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer util.CloseDB(db)
+
+	for i := range tableSQLs {
+		table := newTable()
+		err := parseTableSQL(table, tableSQLs[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = execSQL(db, fmt.Sprintf("drop table %s", table.name))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
