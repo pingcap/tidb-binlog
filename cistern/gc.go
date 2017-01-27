034ce79826f93ffdaeefeb6d37ce17f9909ebc02
iamxy <kenneth.liu.xy@gmail.com>
2017-01-27 13:24:37 +0800

    cistern: batch commit in gc (#113)
    * cistern: batch commit in gc
    
    * cistern: fix loop

--- cistern/gc.go
+++ cistern/gc.go
@@ -36,28 +36,41 @@ func GCHistoryBinlog(store store.Store, ns []byte, duration time.Duration) error
 
 	log.Infof("GC binlog older than timestamp: %v, until date: %v", gcToTS, time.Unix(prevPhysical/1000, (prevPhysical%1000)*1e6))
 
-	batch := store.NewBatch()
-	err = store.Scan(ns, nil, func(key []byte, val []byte) (bool, error) {
-		_, cts, err1 := codec.DecodeInt(key)
-		if err1 != nil {
-			return false, errors.Trace(err1)
-		}
-		if uint64(cts) > gcToTS {
-			return false, nil
-		}
-		if uint64(cts) == gcToTS {
+	var done bool
+	for !done {
+		batch := store.NewBatch()
+		num := 0
+		err = store.Scan(ns, nil, func(key []byte, val []byte) (bool, error) {
+			_, cts, err1 := codec.DecodeInt(key)
+			if err1 != nil {
+				return false, errors.Trace(err1)
+			}
+			if uint64(cts) > gcToTS {
+				done = true
+				return false, nil
+			}
+			if uint64(cts) == gcToTS {
+				return true, nil
+			}
+			batch.Delete(key)
+			num += 1
+			if num > 10000 {
+				return false, nil
+			}
 			return true, nil
+		})
+		if err != nil {
+			return errors.Trace(err)
 		}
-		batch.Delete(key)
-		return true, nil
-	})
-	if err != nil {
-		return errors.Trace(err)
+
+		err = store.Commit(ns, batch)
+		if err != nil {
+			return errors.Trace(err)
+		}
+
+		time.Sleep(50 * time.Millisecond)
 	}
 
-	err = store.Commit(ns, batch)
-	if err != nil {
-		return errors.Trace(err)
-	}
+	log.Infof("FINISHED! GC binlog older than timestamp: %v, until date: %v", gcToTS, time.Unix(prevPhysical/1000, (prevPhysical%1000)*1e6))
 	return nil
 }
