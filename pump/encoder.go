aff1f0183c0a1dfd1f5103bf2e536e12a568b131
WangXiangUSTC <347249478@qq.com>
2017-10-10 14:45:50 +0800

    add info in log

--- pump/encoder.go
+++ pump/encoder.go
@@ -49,7 +49,7 @@ func (k *kafkaEncoder) encode(payload []byte) (int64, error) {
 	msg := &sarama.ProducerMessage{Topic: k.topic, Partition: k.partition, Value: sarama.ByteEncoder(payload)}
 	partition, offset, err := k.producer.SendMessage(msg)
 	if err != nil {
-		log.Infof("send message error: %s, msg: %s, partition: %s, offset: %s, topic: %s", err, msg, partition, offset, topic)
+		log.Infof("send message error: %s, msg: %s, partition: %s, offset: %s, topic: %s", err, msg, partition, offset, k.topic)
 		return 0, errors.Trace(err)
 	}
 
