7cdb39f5a55182a2bbb737947d5cf3b84994ccd6
David Chen <chendahui007@gmail.com>
2018-03-19 16:25:29 +0800

    restore savepoint implementations (#223)

---
+++ restore/savepoint/savepoint.go
@@ -0,0 +1,36 @@
+package savepoint
+
+import (
+	"fmt"
+)
+
+// Position represents a savepoint position .
+type Position struct {
+	Filename string `toml:"filename" json:"filename"`
+	Offset   int64  `toml:"offset" json:"offset"`
+	Ts       int64  `toml:"ts" json:"ts"`
+}
+
+// Savepoint holds the last position.
+type Savepoint interface {
+	// Load loads savepoint position
+	Load() (Position, error)
+	// Save
+	Save(pos Position) error
+	// Flush flushes the position to persistent storage.
+	Flush() error
+	// Pos tells current position
+	Pos() Position
+	// Close the resources the savepoint implementations needs.
+	Close() error
+}
+
+// Open opens a new Savepoint based on the type and path.
+func Open(tp string, path string) (Savepoint, error) {
+	switch tp {
+	case "file":
+		return newFileSavepoint(path)
+	default:
+		panic(fmt.Sprintf("savepoint %s not implemented yet", tp))
+	}
+}
