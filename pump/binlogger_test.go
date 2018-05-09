4966df32f171cf94c89a2a57c3863efcf628eb87
WangXiangUSTC <347249478@qq.com>
2018-05-09 19:30:28 +0800

    address comment

--- pump/binlogger_test.go
+++ pump/binlogger_test.go
@@ -218,6 +218,6 @@ func (s *testBinloggerSuite) TestGC(c *C) {
 
 	names, err := bf.ReadBinlogNames(b.dir)
 	c.Assert(err, IsNil)
-	c.Assert(names, HasLen, 1)
+	c.Assert(names, HasLen, 2)
 	c.Assert(names[0], Equals, bf.BinlogName(1))
 }
