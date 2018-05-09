d0f8a7dbce8d70d88f5ed1d8f3ca3c775130cee3
WangXiangUSTC <347249478@qq.com>
2018-05-09 14:40:22 +0800

    minor fix

--- pump/binlogger.go
+++ pump/binlogger.go
@@ -330,13 +330,15 @@ func (b *binlogger) GC(days time.Duration, pos binlog.Pos) {
 			log.Errorf("parse binlog error %v", err)
 		}
 
-		if curSuffix < pos.Suffix || time.Now().Sub(fi.ModTime()) > days {
+		if curSuffix < pos.Suffix {
 			err := os.Remove(fileName)
 			if err != nil {
 				log.Error("remove old binlog file err")
 				continue
 			}
 			log.Info("GC binlog file:", fileName)
+		} else if time.Now().Sub(fi.ModTime()) > days {
+			log.Warning("binlog file %s is already reach the gc time, but data is not send to kafka", fileName)
 		}
 	}
 }
