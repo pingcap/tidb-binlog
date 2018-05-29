8715337a217f90b767e8b9f77c4e1274c831cecb
Jiahao Huang <july2993@gmail.com>
2018-05-29 19:16:47 +0800

    add bit and set type

--- test/dailytest/case.go
+++ test/dailytest/case.go
@@ -15,6 +15,7 @@ create table  binlog_case1 (
 	t_bigint BIGINT,
 	t_double DOUBLE,
 	t_decimal DECIMAL(38,19),
+	t_bit BIT(64),
 	t_date DATE,
 	t_datetime DATETIME,
 	t_timestamp TIMESTAMP,
@@ -25,19 +26,24 @@ create table  binlog_case1 (
 	t_blob BLOB,
 	t_text TEXT,
 	t_enum ENUM('enum1', 'enum2', 'enum3'),
-	
-
+	t_set SET('a', 'b', 'c'),
 	primary key(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 `,
 	`
-insert into binlog_case1(t_bigint, t_double, t_decimal
+insert into binlog_case1(t_bigint, t_double, t_decimal, t_bit
 	,t_date, t_datetime, t_timestamp, t_time, t_year
-	,t_char, t_varchar, t_blob, t_text, t_enum) values
-	(9223372036854775807, 123.123, 123456789012.123456789012
+	,t_char, t_varchar, t_blob, t_text, t_enum
+	,t_set) values
+	(9223372036854775807, 123.123, 123456789012.123456789012, b'1000001'
 	,'1000-01-01', '9999-12-31 23:59:59', '19731230153000', '23:59:59', 1970
-	,'测', '测试', 'blob', '测试text', 'enum2');
+	,'测', '测试', 'blob', '测试text', 'enum2'
+	,'a,b');
 `,
+}
+
+var case1Clean = []string{`
+	drop table binlog_case1`,
 }
 
 // RunCase run some simple test case
@@ -51,7 +57,7 @@ func RunCase(src *sql.DB, dst *sql.DB) {
 		log.Fatal("src don't equal dst")
 	}
 	// clean table
-	execSQL(src, "drop table binlog_case1;")
+	execSQLs(src, case1Clean)
 	time.Sleep(15 * time.Second)
 	if !util.CheckSyncState(src, dst) {
 		log.Fatal("src don't equal dst")
