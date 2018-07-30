ca1698577abd2d7fe3a9e541b37de5ede275df07
WangXiangUSTC <347249478@qq.com>
2018-07-30 17:13:25 +0800

    add json label

--- pkg/node/node.go
+++ pkg/node/node.go
@@ -90,31 +90,31 @@ func GetState(state string) (State, error) {
 
 // Label is key/value pairs that are attached to objects
 type Label struct {
-	Labels map[string]string
+	Labels map[string]string `json:"labels"`
 }
 
 // Status describes the status information of a tidb-binlog node in etcd.
 type Status struct {
 	// the id of node.
-	NodeID string
+	NodeID string `json:"nodeId"`
 
 	// the host of pump or node.
-	Host string
+	Host string `json:"host"`
 
 	// the state of pump.
-	State State
+	State State `json:"state"`
 
 	// the node is alive or not.
-	IsAlive bool
+	IsAlive bool `json:"isAlive"`
 
 	// the score of node, it is report by node, calculated by node's qps, disk usage and binlog's data size.
 	// if Score is less than 0, means this node is useless. Now only used for pump.
-	Score int64
+	Score int64 `json:"score"`
 
 	// the label of this node. Now only used for pump.
 	// pump client will only send to a pump which label is matched.
-	Label *Label
+	Label *Label `json:"label"`
 
 	// UpdateTS is the last update ts of node's status.
-	UpdateTS int64
+	UpdateTS int64 `json:"updateTS"`
 }
