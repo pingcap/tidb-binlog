cd2d708a0691304a4c9c94c58f490a9700d464ea
Jiahao Huang <july2993@gmail.com>
2018-08-16 16:52:43 +0800

    binlogger.go:/ let ReadFrom call Walk, avoid duplicate code

--- pump/binlogger.go
+++ pump/binlogger.go
@@ -151,103 +151,42 @@ func CloseBinlogger(binlogger Binlogger) error {
 // ReadFrom reads `nums` binlogs from the given binlog position
 // read all binlogs from one file then close it and open the following file
 func (b *binlogger) ReadFrom(from binlog.Pos, nums int32) ([]binlog.Entity, error) {
-	var ent = &binlog.Entity{}
-	var ents = []binlog.Entity{}
-	var index int32
-	var decoder Decoder
-	var first = true
-
-	dirpath := b.dir
-
 	if nums < 0 {
 		return nil, errors.Errorf("read number must be positive")
 	}
 
-	names, err := bf.ReadBinlogNames(b.dir)
-	if err != nil {
-		corruptionBinlogCounter.Add(1)
-		return nil, errors.Trace(err)
+	inums := int(nums)
+	var ents []binlog.Entity
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	err := b.Walk(ctx, from, func(entity *binlog.Entity) error {
+		if len(ents) < inums {
+			var dup binlog.Entity
+			dup.Pos = entity.Pos
+			dup.Payload = make([]byte, len(entity.Payload))
+			copy(dup.Payload, entity.Payload)
+			dup.Checksum = make([]byte, len(entity.Checksum))
+			copy(dup.Checksum, entity.Checksum)
+
+			ents = append(ents, dup)
+		} else if len(ents) == inums {
+			cancel()
+		}
+
+		return nil
+	})
+
+	// may encounter err after read enough entity
+	if len(ents) == inums {
+		err = nil
 	}
 
-	nameIndex, ok := bf.SearchIndex(names, from.Suffix)
-	if !ok {
-		corruptionBinlogCounter.Add(1)
-		return nil, errors.Annotatef(bf.ErrFileNotFound, "can not find index %d", from.Suffix)
-	}
-
-	for _, name := range names[nameIndex:] {
-		p := path.Join(dirpath, name)
-		f, err := os.OpenFile(p, os.O_RDONLY, file.PrivateFileMode)
-		if err != nil {
-			readErrorCounter.WithLabelValues("local").Add(1)
-			return ents, errors.Trace(err)
-		}
-		defer f.Close()
-
-		if first {
-			first = false
-			_, err := f.Seek(from.Offset, io.SeekStart)
-			if err != nil {
-				readErrorCounter.WithLabelValues("local").Add(1)
-				return ents, errors.Trace(err)
-			}
-		}
-
-		decoder = NewDecoder(from, io.Reader(f))
-
-		for index < nums {
-			beginTime := time.Now()
-			err = decoder.Decode(ent, &binlogBuffer{})
-			if err != nil {
-				if err == ErrCRCMismatch || err == ErrMagicMismatch {
-					corruptionBinlogCounter.Add(1)
-					log.Errorf("decode %+v binlog error %v", from, err)
-					// offset + 1 to skip magic code of current binlog
-					offset, err1 := seekBinlog(f, from.Offset+1)
-					if err1 == nil {
-						from.Offset = offset
-						decoder = NewDecoder(from, io.Reader(f))
-						continue
-					}
-					if err1 == io.EOF || err1 == io.ErrUnexpectedEOF {
-						err = io.EOF
-					} else {
-						err = errors.Annotatef(err1, "decode %+v binlog error %v, and fail to seek next magic", from, err)
-						log.Error(err)
-					}
-				}
-				break
-			}
-			readBinlogHistogram.WithLabelValues("local").Observe(time.Since(beginTime).Seconds())
-
-			from.Offset = ent.Pos.Offset
-			newEnt := binlog.Entity{
-				Pos:     ent.Pos,
-				Payload: ent.Payload,
-			}
-			ents = append(ents, newEnt)
-			index++
-		}
-
-		if err != nil && err != io.EOF {
-			readErrorCounter.WithLabelValues("local").Add(1)
-			log.Errorf("read from local binlog file %d error %v", from.Suffix, err)
-			return ents, err
-		}
-
-		// read enough binlogs
-		if index == nums {
-			return ents, nil
-		}
-
-		from.Suffix++
-		from.Offset = 0
-	}
-
-	return ents, nil
+	return ents, err
 }
 
 // Walk reads binlog from the "from" position and sends binlogs in the streaming way
+// Note: entity is not safe to use after sendBinlog has return
 func (b *binlogger) Walk(ctx context.Context, from binlog.Pos, sendBinlog func(entity *binlog.Entity) error) error {
 	log.Infof("[binlogger] walk from position %+v", from)
 	var (
