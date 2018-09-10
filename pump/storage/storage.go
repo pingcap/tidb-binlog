32a0e5582a4cd6eb1055f5c0f1d15bb101c57114
Jiahao Huang <july2993@gmail.com>
2018-09-10 14:39:33 +0800

    storage.go: fix getStorageSize

--- pump/storage/storage.go
+++ pump/storage/storage.go
@@ -6,6 +6,7 @@ import (
 	"math"
 	"os"
 	"path"
+	"reflect"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -925,19 +926,29 @@ type storageSize struct {
 func getStorageSize(dir string) (size storageSize, err error) {
 	var stat unix.Statfs_t
 
-	wd, err := os.Getwd()
+	err = unix.Statfs(dir, &stat)
 	if err != nil {
 		return size, errors.Trace(err)
 	}
 
-	err = unix.Statfs(wd, &stat)
-	if err != nil {
-		return size, errors.Trace(err)
+	// When container is run in MacOS, `bsize` obtained by `statfs` syscall is not the fundamental block size,
+	// but the `iosize` (optimal transfer block size) instead, it's usually 1024 times larger than the `bsize`.
+	// for example `4096 * 1024`. To get the correct block size, we should use `frsize`. But `frsize` isn't
+	// guaranteed to be supported everywhere, so we need to check whether it's supported before use it.
+	// For more details, please refer to: https://github.com/docker/for-mac/issues/2136
+	bSize := uint64(stat.Bsize)
+	field := reflect.ValueOf(&stat).Elem().FieldByName("Frsize")
+	if field.IsValid() {
+		if field.Kind() == reflect.Uint64 {
+			bSize = field.Uint()
+		} else {
+			bSize = uint64(field.Int())
+		}
 	}
 
 	// Available blocks * size per block = available space in bytes
-	size.available = int(stat.Bavail) * int(stat.Bsize)
-	size.capacity = int(stat.Blocks) * int(stat.Bsize)
+	size.available = int(stat.Bavail) * int(bSize)
+	size.capacity = int(stat.Blocks) * int(bSize)
 
 	return
 }
