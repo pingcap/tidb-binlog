dd92a0f2d97ff82271cd2cbd66423b2e7d499908
Jiahao Huang <july2993@gmail.com>
2018-11-07 13:16:29 +0800

    drainer/* config the schema name to save checkpoint table (#381)
    * drainer/* config the schema name to save checkpoint table
    
    for some user want to run multi drainer for different DB

--- drainer/executor/util.go
+++ drainer/executor/util.go
@@ -7,14 +7,15 @@ import (
 
 // DBConfig is the DB configuration.
 type DBConfig struct {
-	Host          string `toml:"host" json:"host"`
-	User          string `toml:"user" json:"user"`
-	Password      string `toml:"password" json:"password"`
-	Port          int    `toml:"port" json:"port"`
-	BinlogFileDir string `toml:"dir" json:"dir"`
-	Compression   string `toml:"compression" json:"compression"`
-	TimeLimit     string `toml:"time-limit" json:"time-limit"`
-	SizeLimit     string `toml:"size-limit" json:"size-limit"`
+	Host          string           `toml:"host" json:"host"`
+	User          string           `toml:"user" json:"user"`
+	Password      string           `toml:"password" json:"password"`
+	Port          int              `toml:"port" json:"port"`
+	Checkpoint    CheckpointConfig `toml:"checkpoint" json:"checkpoint"`
+	BinlogFileDir string           `toml:"dir" json:"dir"`
+	Compression   string           `toml:"compression" json:"compression"`
+	TimeLimit     string           `toml:"time-limit" json:"time-limit"`
+	SizeLimit     string           `toml:"size-limit" json:"size-limit"`
 
 	ZKAddrs      string `toml:"zookeeper-addrs" json:"zookeeper-addrs"`
 	KafkaAddrs   string `toml:"kafka-addrs" json:"kafka-addrs"`
@@ -22,4 +23,9 @@ type DBConfig struct {
 	TopicName    string `toml:"topic-name" json:"topic-name"`
 	// get it from pd
 	ClusterID uint64 `toml:"-" json:"-"`
+}
+
+// CheckpointConfig is the Checkpoint configuration.
+type CheckpointConfig struct {
+	Schema string `toml:"schema" json:"schema"`
 }
