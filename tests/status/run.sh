ec3e11ca2a12d6207e36761aa31087c742d58c88
WangXiangUSTC <347249478@qq.com>
2018-12-14 20:21:10 +0800

    fix test

--- tests/status/run.sh
+++ tests/status/run.sh
@@ -9,16 +9,20 @@ wget download.pingcap.org/tidb-tools-v2.1.0-linux-amd64.tar.gz
 tar zxvf tidb-tools-v2.1.0-linux-amd64.tar.gz
 
 statusLog="status.log"
-./tidb-tools-v2.1.0-linux-amd64/bin/binlogctl -pd-urls 127.0.0.1:2379 -cmd pumps > $statusLog
+# pump's state should be online
+./tidb-tools-v2.1.0-linux-amd64/bin/binlogctl -pd-urls 127.0.0.1:2379 -cmd pumps > $statusLog 2>&1
 if ! grep -Fq "online" $statusLog; then
+    cat $statusLog
     echo "pump is not online" 
 	exit 2
 fi
 
+# stop pump, and pump's state should be paused
 ./tidb-tools-v2.1.0-linux-amd64/bin/binlogctl -pd-urls 127.0.0.1:2379 -cmd stop-pump -node-id pump1:8215
 sleep 2
-./tidb-tools-v2.1.0-linux-amd64/bin/binlogctl -pd-urls 127.0.0.1:2379 -cmd pumps > $statusLog
+./tidb-tools-v2.1.0-linux-amd64/bin/binlogctl -pd-urls 127.0.0.1:2379 -cmd pumps > $statusLog 2>&1
 if ! grep -Fq "paused" $statusLog; then
+    cat $statusLog
     echo "pump is not paused"
 	exit 2
 fi
@@ -27,18 +31,21 @@ run_drainer &
 
 sleep 2
 
-killall drainer || true
-
 ./tidb-tools-v2.1.0-linux-amd64/bin/binlogctl -pd-urls 127.0.0.1:2379 -cmd stop-drainer -node-id drainer:123
 
 sleep 2
 
-./tidb-tools-v2.1.0-linux-amd64/bin/binlogctl -pd-urls 127.0.0.1:2379 -cmd drainers > $statusLog
+# stop drainer, and drainer's state should be paused
+./tidb-tools-v2.1.0-linux-amd64/bin/binlogctl -pd-urls 127.0.0.1:2379 -cmd drainers > $statusLog 2>&1
 if ! grep -Fq "paused" $statusLog; then
+    cat $statusLog
     echo "drainer is not paused"
 	exit 2
 fi
 
+kill all pump || true
+
+# update drainer's state to online, and then run pump, pump should exit with error.
 ./tidb-tools-v2.1.0-linux-amd64/bin/binlogctl -pd-urls 127.0.0.1:2379 -cmd update-drainer -node-id drainer:123 -state online
 
 run_pump
