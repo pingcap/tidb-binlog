8cf485a43cdcfcf7ebf6ff6defdfbac15350d61c
WangXiangUSTC <xiang13225080@163.com>
2019-02-22 17:24:47 +0800

    reparo: add `` for column (#468)

--- tests/reparo/run.sh
+++ tests/reparo/run.sh
@@ -6,15 +6,15 @@ cd "$(dirname "$0")"
 
 run_drainer &
 
-run_sql "DROP DATABASE IF EXISTS reparo_test;"
-run_sql "CREATE DATABASE reparo_test"
-run_sql "CREATE TABLE reparo_test.test(id int, name varchar(10), PRIMARY KEY(id))"
+run_sql "DROP DATABASE IF EXISTS \`reparo_test\`;"
+run_sql "CREATE DATABASE \`reparo_test\`"
+run_sql "CREATE TABLE \`reparo_test\`.\`test\`(\`id\` int, \`name\` varchar(10), \`all\` varchar(10), PRIMARY KEY(\`id\`))"
 
-run_sql "INSERT INTO reparo_test.test VALUES(1, 'a'), (2, 'b')"
-run_sql "INSERT INTO reparo_test.test VALUES(3, 'c'), (4, 'd')"
-run_sql "UPDATE reparo_test.test SET name = 'bb' where id = 2"
-run_sql "DELETE FROM reparo_test.test WHERE name = 'bb'"
-run_sql "INSERT INTO reparo_test.test VALUES(5, 'e')"
+run_sql "INSERT INTO \`reparo_test\`.\`test\` VALUES(1, 'a', 'a'), (2, 'b', 'b')"
+run_sql "INSERT INTO \`reparo_test\`.\`test\` VALUES(3, 'c', 'c'), (4, 'd', 'c')"
+run_sql "UPDATE \`reparo_test\`.\`test\` SET \`name\` = 'bb' where \`id\` = 2"
+run_sql "DELETE FROM \`reparo_test\`.\`test\` WHERE \`name\` = 'bb'"
+run_sql "INSERT INTO \`reparo_test\`.\`test\` VALUES(5, 'e', 'e')"
 
 sleep 5
 
@@ -22,10 +22,10 @@ run_reparo &
 
 sleep 5
 
-down_run_sql "SELECT count(*) FROM reparo_test.test"
+down_run_sql "SELECT count(*) FROM \`reparo_test\`.\`test\`"
 check_contains "count(*): 4"
 
 # clean up
-run_sql "DROP DATABASE IF EXISTS reparo_test"
+run_sql "DROP DATABASE IF EXISTS \`reparo_test\`"
 
 killall drainer
