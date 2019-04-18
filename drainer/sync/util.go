0fa70021a0d29cdcda00aecb8531cad80868d4b2
Xiaoguang Sun <sunxiaoguang@zhihu.com>
2019-04-18 15:49:23 +0800

    Support choosing partition by schema or table
    Support choosing partition by schema or table if users explicitly say so
    otherwise use fixed partition 0.
    
    Signed-off-by: Xiaoguang Sun <sunxiaoguang@zhihu.com>

--- drainer/sync/util.go
+++ drainer/sync/util.go
@@ -29,11 +29,12 @@ type DBConfig struct {
 	TimeLimit     string           `toml:"time-limit" json:"time-limit"`
 	SizeLimit     string           `toml:"size-limit" json:"size-limit"`
 
-	ZKAddrs          string `toml:"zookeeper-addrs" json:"zookeeper-addrs"`
-	KafkaAddrs       string `toml:"kafka-addrs" json:"kafka-addrs"`
-	KafkaVersion     string `toml:"kafka-version" json:"kafka-version"`
-	KafkaMaxMessages int    `toml:"kafka-max-messages" json:"kafka-max-messages"`
-	TopicName        string `toml:"topic-name" json:"topic-name"`
+	ZKAddrs            string `toml:"zookeeper-addrs" json:"zookeeper-addrs"`
+	KafkaAddrs         string `toml:"kafka-addrs" json:"kafka-addrs"`
+	KafkaVersion       string `toml:"kafka-version" json:"kafka-version"`
+	KafkaMaxMessages   int    `toml:"kafka-max-messages" json:"kafka-max-messages"`
+	TopicName          string `toml:"topic-name" json:"topic-name"`
+	KafkaPartitionMode string `toml:"kafka-partition-mode" json:"kafka-partition-mode"`
 	// get it from pd
 	ClusterID uint64 `toml:"-" json:"-"`
 }
