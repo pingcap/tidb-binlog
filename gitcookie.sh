e014f97eb9a3429508aa6e9985549d27cf4b0e79
zhouqiang <zhouqiang.cl@gmail.com>
2019-04-18 14:49:33 +0800

    Update gitcookie.sh

--- gitcookie.sh
+++ gitcookie.sh
@@ -4,6 +4,7 @@ chmod 0600 ~/.gitcookies
 
 git config --global http.cookiefile ~/.gitcookies
 
+
 tr , \\t <<\__END__ >>~/.gitcookies
 go.googlesource.com,FALSE,/,TRUE,2147483647,o,git-shenli.pingcap.com=1/rGvVlvFq_x9rxOmXqQe_rfcrjbOk6NSOHIQKhhsfidM
 go-review.googlesource.com,FALSE,/,TRUE,2147483647,o,git-shenli.pingcap.com=1/rGvVlvFq_x9rxOmXqQe_rfcrjbOk6NSOHIQKhhsfidM
