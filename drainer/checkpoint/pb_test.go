0b3b448d3fcd4504358fe44fe75eb120f7f5c576
mahjonp <junpeng.man@gmail.com>
2019-04-21 12:14:55 +0800

    change test file to /tmp (#443)

--- drainer/checkpoint/pb_test.go
+++ drainer/checkpoint/pb_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func (t *testCheckPointSuite) TestPb(c *C) {
-	fileName := "test"
+	fileName := "/tmp/test"
 	notExistFileName := "test_not_exist"
 	cfg := new(Config)
 	cfg.CheckPointFile = fileName
