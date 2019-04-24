4cc5317fc2e0700d72058794a0e33495bcf8eba1
WangXiangUSTC <xiang13225080@163.com>
2019-04-24 12:12:51 +0800

    add test for callback

--- reparo/syncer/mysql_test.go
+++ reparo/syncer/mysql_test.go
@@ -1,6 +1,8 @@
 package syncer
 
 import (
+	"time"
+
 	sqlmock "github.com/DATA-DOG/go-sqlmock"
 	"github.com/pingcap/check"
 	pb "github.com/pingcap/tidb-binlog/proto/binlog"
@@ -27,10 +29,16 @@ func (s *testMysqlSuite) TestMysqlSyncer(c *check.C) {
 
 	mock.ExpectBegin()
 	mock.ExpectExec("create database test").WillReturnResult(sqlmock.NewResult(0, 0))
- 	mock.ExpectCommit()
+	mock.ExpectCommit()
 
-	err = syncer.Sync(ddlBinlog, func(binlog *pb.Binlog) {})
+	binlogs := make([]*pb.Binlog, 0, 1)
+	err = syncer.Sync(ddlBinlog, func(binlog *pb.Binlog) {
+		c.Log(binlog)
+		binlogs = append(binlogs, binlog)
+	})
 	c.Assert(err, check.IsNil)
+	time.Sleep(100 * time.Millisecond)
+	c.Assert(binlogs, check.HasLen, 1)
 
 	err = syncer.Sync(dmlBinlog, func(binlog *pb.Binlog) {})
 	c.Assert(err, check.IsNil)
