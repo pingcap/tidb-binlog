e3aa11b5449669419e7f16a2d4ece59a92615383
WangXiangUSTC <xiang13225080@163.com>
2019-04-28 17:43:33 +0800

    add comment

--- binlogctl/meta_test.go
+++ binlogctl/meta_test.go
@@ -45,5 +45,6 @@ func (s *metaSuite) TestSaveMeta(c *C) {
 	c.Assert(lines, HasLen, 3)
 	c.Assert(lines[0], Equals, "commitTS = 123")
 	c.Assert(lines[1], Equals, "1970-01-01 00:00:00 +0000 UTC")
-	c.Assert(lines[2], Matches, "1970-01-01.*")
+	// the output depends on the local's timezone
+	c.Assert(lines[2], Matches, "1970-01-0.*")
 }
