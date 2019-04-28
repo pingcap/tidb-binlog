cd120215965b55ddbd80ee6c9da3cda3ad8d46be
WangXiangUSTC <xiang13225080@163.com>
2019-04-28 14:31:09 +0800

    refine test

--- binlogctl/nodes_test.go
+++ binlogctl/nodes_test.go
@@ -14,11 +14,12 @@
 package binlogctl
 
 import (
-	"fmt"
 	"time"
 	"testing"
 	"path"
 	"context"
+	"net/http"
+	"net/http/httptest"
 
 	"github.com/coreos/etcd/integration"
 	"github.com/pingcap/tidb-binlog/pkg/etcd"
@@ -28,86 +29,112 @@ import (
 
 type nodesSuite struct{}
 
-var _ = Suite(&nodesSuite{})
-var testEtcdCluster *integration.ClusterV3
-var fakeRegistry *node.EtcdRegistry
+var (
+	_ = Suite(&nodesSuite{})
+	testEtcdCluster *integration.ClusterV3
+	fakeRegistry *node.EtcdRegistry
+)
 
 func TestNode(t *testing.T) {
 	testEtcdCluster = integration.NewClusterV3(t, &integration.ClusterConfig{Size: 1})
 	defer testEtcdCluster.Terminate(t)
 
-	createFakeRegistry("127.0.0.1:2379")
+	createMockRegistry("127.0.0.1:2379")
 	TestingT(t)
 }
 
 func (s *nodesSuite) TestApplyAction(c *C) {
-	err := ApplyAction("127.0.0.1:2379", "pump", "nodeID", PausePump)
-	c.Assert(err, NotNil)
-}
-
-func (s *nodesSuite) TestQueryNodesByKind(c *C) {
-	createRegistryFuc = createFakeRegistry
+	createRegistryFuc = createMockRegistry
 	defer func() {
 		createRegistryFuc = createRegistry
 	}()
 
-	ns := &node.Status{
-		NodeID:  "test",
-		Addr:    "test",
-		State:   node.Online,
-		IsAlive: true,
-	}
+	url := createMockPumpServer(c)
 
-	c.Log("insert node")
-	nodePrefix := path.Join(node.DefaultRootPath, node.NodePrefix["pumps"])
-	err := fakeRegistry.UpdateNode(context.Background(), nodePrefix, ns)
+	err := ApplyAction("127.0.0.1:2379", "pumps", "test2", PausePump)
+	c.Assert(err, ErrorMatches, "nodeID test2 not found")
+
+	registerPumpForTest(c, "test", url)
+	// TODO: read log infomarion and add check
+	err = ApplyAction("127.0.0.1:2379", "pumps", "test", PausePump)
 	c.Assert(err, IsNil)
+}
 
-	c.Log("query node")
-	err = QueryNodesByKind("127.0.0.1:2379", "pumps")
+func (s *nodesSuite) TestQueryNodesByKind(c *C) {
+	createRegistryFuc = createMockRegistry
+	defer func() {
+		createRegistryFuc = createRegistry
+	}()
+
+	registerPumpForTest(c, "test", "127.0.0.1:8255")
+
+	// TODO: read log infomarion and add check
+	err := QueryNodesByKind("127.0.0.1:2379", "pumps")
 	c.Assert(err, IsNil)
 }
 
 func (s *nodesSuite) TestUpdateNodeState(c *C) {
-	createRegistryFuc = createFakeRegistry
+	createRegistryFuc = createMockRegistry
 	defer func() {
 		createRegistryFuc = createRegistry
 	}()
 
+	registerPumpForTest(c, "test", "127.0.0.1:8255")
+
+	err := UpdateNodeState("127.0.0.1:2379", "pumps", "test", node.Paused)
+	c.Assert(err, IsNil)
+
+	// check node's state is changed to paused
+	nodePrefix := path.Join(node.DefaultRootPath, node.NodePrefix["pumps"])
+	nodes, err := fakeRegistry.Nodes(context.Background(), nodePrefix)
+	c.Assert(err, IsNil)
+	var findNode bool
+	for _, n := range nodes {
+		if n.NodeID == "test" {
+			findNode = true
+			c.Assert(n.State, Equals, node.Paused)
+		}
+	}
+	c.Assert(findNode, IsTrue)
+}
+
+func createMockRegistry(urls string) (*node.EtcdRegistry, error) {
+	if fakeRegistry != nil {
+		return fakeRegistry, nil
+	}
+
+	etcdclient := etcd.NewClient(testEtcdCluster.RandClient(), node.DefaultRootPath)
+	fakeRegistry = node.NewEtcdRegistry(etcdclient, time.Duration(5)*time.Second)
+	
+	return fakeRegistry, nil
+}
+
+func registerPumpForTest(c *C, nodeID, address string) {
 	ns := &node.Status{
-		NodeID:  "test",
-		Addr:    "test",
+		NodeID:  nodeID,
+		Addr:    address,
 		State:   node.Online,
 		IsAlive: true,
 	}
 
 	nodePrefix := path.Join(node.DefaultRootPath, node.NodePrefix["pumps"])
 	err := fakeRegistry.UpdateNode(context.Background(), nodePrefix, ns)
-	c.Assert(err, IsNil)
-
-	err = UpdateNodeState("127.0.0.1:2379", "pumps", "test", node.Paused)
-	c.Assert(err, IsNil)
+	if err != nil {
+		c.Fatal(err)
+	}
 }
 
-func createFakeRegistry(urls string) (*node.EtcdRegistry, error) {
-	if fakeRegistry != nil {
-		return fakeRegistry, nil
-	}
+type httpHandler struct {
+}
 
-	fmt.Println("create etcd client")
-	etcdclient := etcd.NewClient(testEtcdCluster.RandClient(), node.DefaultRootPath)
-	fmt.Println("create registry")
-	fakeRegistry = node.NewEtcdRegistry(etcdclient, time.Duration(5)*time.Second)
-	
-	return fakeRegistry, nil
-	/*
-	ns := &Status{
-		NodeID:  "test",
-		Addr:    "test",
-		State:   Online,
-		IsAlive: true,
-	}
-	err := r.UpdateNode(context.Background(), nodePrefix, ns)
-	c.Assert(err, IsNil)
-	*/
+func (h *httpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	return
+}
+
+func createMockPumpServer(c *C) string {
+	handler := &httpHandler{}
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	return server.URL
 }
