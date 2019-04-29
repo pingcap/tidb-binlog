c274caaffcc26927d9153e1c412dee71290eb1a0
WangXiangUSTC <xiang13225080@163.com>
2019-04-29 11:39:22 +0800

    address comment

--- reparo/syncer/syncer_test.go
+++ reparo/syncer/syncer_test.go
@@ -1,6 +1,8 @@
 package syncer
 
 import (
+	"reflect"
+
 	"github.com/pingcap/check"
 )
 
@@ -11,18 +13,29 @@ var _ = check.Suite(&testSyncerSuite{})
 func (s *testSyncerSuite) TestNewSyncer(c *check.C) {
 	cfg := new(DBConfig)
 
-	syncer, err := New("print", cfg)
-	c.Assert(err, check.IsNil)
-	_, ok := syncer.(*printSyncer)
-	c.Assert(ok, check.IsTrue)
+	testCases := []struct {
+		typeStr string
+		tp      reflect.Type
+		checker check.Checker
+	}{
+		{
+			"print",
+			reflect.TypeOf(new(printSyncer)),
+			check.Equals,
+		}, {
+			"memory",
+			reflect.TypeOf(new(MemSyncer)),
+			check.Equals,
+		}, {
+			"print",
+			reflect.TypeOf(new(MemSyncer)),
+			check.Not(check.Equals),
+		},
+	}
 
-	syncer, err = New("memory", cfg)
-	c.Assert(err, check.IsNil)
-	_, ok = syncer.(*MemSyncer)
-	c.Assert(ok, check.IsTrue)
-
-	syncer, err = New("print", cfg)
-	c.Assert(err, check.IsNil)
-	_, ok = syncer.(*MemSyncer)
-	c.Assert(ok, check.IsFalse)
+	for _, testCase := range testCases {
+		syncer, err := New(testCase.typeStr, cfg)
+		c.Assert(err, check.IsNil)
+		c.Assert(reflect.TypeOf(syncer), testCase.checker, testCase.tp)
+	}
 }
