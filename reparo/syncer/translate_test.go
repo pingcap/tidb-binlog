4c01bd123cf4e6e408f3a799e9cc93dbeddbfb05
WangXiangUSTC <xiang13225080@163.com>
2019-05-04 22:48:27 +0800

    format code

--- reparo/syncer/translate_test.go
+++ reparo/syncer/translate_test.go
@@ -16,22 +16,22 @@ var _ = check.Suite(&testTranslateSuite{})
 
 func (s *testTranslateSuite) TestPBBinlogToTxn(c *check.C) {
 	tests := map[*pb.Binlog]*loader.Txn{
-		&pb.Binlog{
+		{
 			Tp:       pb.BinlogType_DDL,
 			DdlQuery: []byte("use db1; create table table1(id int)"),
-		}: &loader.Txn{
+		}: {
 			DDL: &loader.DDL{
 				SQL: "use db1; create table table1(id int)",
 			},
 		},
 		// TODO add dml test
-		&pb.Binlog{
+		{
 			Tp: pb.BinlogType_DML,
 			DmlData: &pb.DMLData{
 				Events: []pb.Event{},
 			},
-		}: &loader.Txn{
-		// DMLs: []*loader.DML{},
+		}: {
+			// DMLs: []*loader.DML{},
 		},
 	}
 
