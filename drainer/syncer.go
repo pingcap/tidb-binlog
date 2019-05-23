382287edf3d1c393f97ddc3e582122761e5d95a6
satoru <satorulogic@gmail.com>
2019-05-23 17:33:06 +0800

    drainer: Add a metric to track the delay of the downstream (#594)

--- drainer/syncer.go
+++ drainer/syncer.go
@@ -221,6 +221,8 @@ func (s *Syncer) handleSuccess(fakeBinlog chan *pb.Binlog, lastTS *int64) {
 				lastSaveTS = ts
 				eventCounter.WithLabelValues("savepoint").Add(1)
 			}
+			delay := oracle.GetPhysical(time.Now()) - oracle.ExtractPhysical(uint64(ts))
+			checkpointDelayHistogram.Observe(float64(delay) / 1e3)
 		}
 	}
 
