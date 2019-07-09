8b1e5bde976da233ddbcdd4ef36291ef59b421ba
WangXiangUSTC <xiang13225080@163.com>
2019-07-09 12:20:01 +0800

    update test

--- pkg/loader/load_test.go
+++ pkg/loader/load_test.go
@@ -498,14 +498,22 @@ func (ms *markSuccessesSuite) TestShouldSetFinishTS(c *check.C) {
 	loader.markSuccess(txns...)
 	c.Assert(txns[len(txns)-1].FinishTS, check.Equals, fGetFinishTS(nil))
 
+	txns = []*Txn{
+		{Metadata: 7},
+		{Metadata: 8},
+	}
+	loader.markSuccess(txns...)
+	c.Assert(txns[len(txns)-1].FinishTS, check.Equals, int64(0))
+
+
 	originUpdateLastFinishTSInterval := updateLastFinishTSInterval
 	updateLastFinishTSInterval = 0
 	defer func() {
 		updateLastFinishTSInterval = originUpdateLastFinishTSInterval
 	}()
 	txns = []*Txn{
-		{Metadata: 7},
-		{Metadata: 8},
+		{Metadata: 9},
+		{Metadata: 10},
 	}
 	loader.markSuccess(txns...)
 	c.Assert(txns[len(txns)-1].FinishTS, check.Equals, int64(88881234))
