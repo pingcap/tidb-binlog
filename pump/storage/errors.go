580ad90be5d36c7871960c76071815ea3e585cc7
Jiahao Huang <july2993@gmail.com>
2019-07-20 01:14:46 +0800

    pump/: Refine log when no available space (#681)

--- pump/storage/errors.go
+++ pump/storage/errors.go
@@ -18,7 +18,4 @@ import "github.com/pingcap/errors"
 var (
 	// ErrWrongMagic means the magic number mismatch
 	ErrWrongMagic = errors.New("wrong magic")
-
-	// ErrNoAvailableSpace means no available space
-	ErrNoAvailableSpace = errors.New("no available space")
 )
