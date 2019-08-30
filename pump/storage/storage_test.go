c93c19060bdedfc325d614e556fe8c33c9eb48ac
leoppro <i@leop.pro>
2019-08-30 09:22:16 +0800

    Update pump/storage/storage_test.go

--- pump/storage/storage_test.go
+++ pump/storage/storage_test.go
@@ -277,7 +277,7 @@ func (as *AppendSuit) TestDoGCTS(c *check.C) {
 	defer cleanAppend(append)
 
 	var i int64
-	var n int64 = 1024 * 500
+	var n int64 = 1025 * 500
 	var batch leveldb.Batch
 	for i = 1; i < n; i++ {
 		batch.Put(encodeTSKey(i), value)
