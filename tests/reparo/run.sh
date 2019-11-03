977b02144b238695d7222cede0700593a3aea380
Jiahao Huang <july2993@gmail.com>
2019-11-03 13:30:51 +0800

    corect create db order
    don't rely on privier drainer instance

--- tests/reparo/run.sh
+++ tests/reparo/run.sh
@@ -7,13 +7,14 @@ cd "$(dirname "$0")"
 # use latest ts as initial-commit-ts, so we can skip binlog by previous test case
 args="-initial-commit-ts=-1"
 down_run_sql "DROP DATABASE IF EXISTS tidb_binlog"
-run_sql "CREATE DATABASE IF NOT EXISTS \`reparo_test\`"
 
 rm -rf /tmp/tidb_binlog_test/data.drainer
 
 run_drainer "$args" &
 
 GO111MODULE=on go build -o out
+
+run_sql "CREATE DATABASE IF NOT EXISTS \`reparo_test\`"
 
 ./out -config ./config.toml > ${OUT_DIR-/tmp}/$TEST_NAME.out 2>&1
 
