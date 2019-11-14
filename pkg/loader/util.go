3f0d148e33777caa1e721000999e84acaebefc28
leoppro <zhaoyilin@pingcap.com>
2019-11-14 16:55:37 +0800

    loader: add a log when table not found (#809)

--- pkg/loader/util.go
+++ pkg/loader/util.go
@@ -57,10 +57,7 @@ func getTableInfo(db *gosql.DB, schema string, table string) (info *tableInfo, e
 	info = new(tableInfo)
 
 	if info.columns, err = getColsOfTbl(db, schema, table); err != nil {
-		if err == ErrTableNotExist {
-			return nil, err
-		}
-		return nil, errors.Trace(err)
+		return nil, errors.Annotatef(err, "table `%s`.`%s`", schema, table)
 	}
 
 	if info.uniqueKeys, err = getUniqKeys(db, schema, table); err != nil {
