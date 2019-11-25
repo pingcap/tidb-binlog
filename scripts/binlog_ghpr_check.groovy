d565e1ba9cc096c3a4d890a68b3609c783b7617d
zhaoyixuan <johnsonqrr@gmail.com>
2019-11-25 12:41:03 +0800

    add binlog_ghpr scripts from jenkins

---
+++ scripts/binlog_ghpr_check.groovy
@@ -0,0 +1,54 @@
+
+catchError {
+    node("build_go1120_binlog") {
+        deleteDir()
+        def ws = pwd()
+        container("golang") {
+            dir("/home/jenkins/agent/git/tidb-binlog") {
+                if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                    deleteDir()
+                }
+                try {
+                    checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: '+refs/pull/*:refs/remotes/origin/pr/*', url: 'git@github.com:pingcap/tidb-binlog.git']]]
+                } catch (error) {
+                    retry(2) {
+                        echo "checkout failed, retry.."
+                        sleep 60
+                        if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                            deleteDir()
+                        }
+                        checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: '+refs/pull/*:refs/remotes/origin/pr/*', url: 'git@github.com:pingcap/tidb-binlog.git']]]
+                    }
+                }
+            }
+
+            dir("go/src/github.com/pingcap/tidb-binlog") {
+
+                stage('Build') {
+                    sh """
+                        cp -R /home/jenkins/agent/git/tidb-binlog/. ./
+                        git checkout -f ${ghprbActualCommit}
+                        sudo rm -rf /go/pkg/mod/cache
+                        GOPATH=\$GOPATH:${ws}/go make check
+                    """
+                }
+            }
+        }
+    }
+
+    currentBuild.result = "SUCCESS"
+}
+
+stage('Summary') {
+    def duration = ((System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000 / 60).setScale(2, BigDecimal.ROUND_HALF_UP)
+    def slackmsg = "[#${ghprbPullId}: ${ghprbPullTitle}]" + "\n" +
+    "${ghprbPullLink}" + "\n" +
+    "${ghprbPullDescription}" + "\n" +
+    "Build Result: `${currentBuild.result}`" + "\n" +
+    "Elapsed Time: `${duration} mins` " + "\n" +
+    "${env.RUN_DISPLAY_URL}"
+
+    if (currentBuild.result != "SUCCESS") {
+        slackSend channel: '#jenkins-ci', color: 'danger', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+    }
+}
