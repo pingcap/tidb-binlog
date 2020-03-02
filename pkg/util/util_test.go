211cda944c7c5fc9a32b780a002e180ab60f9cf8
lysu <sulifx@gmail.com>
2020-03-02 20:03:57 +0800

    *: update pd dependence version

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -24,7 +24,7 @@ import (
 	"github.com/pingcap/log"
 	"github.com/pingcap/parser/model"
 	"github.com/pingcap/parser/mysql"
-	pd "github.com/pingcap/pd/client"
+	pd "github.com/pingcap/pd/v4/client"
 	"github.com/pingcap/tidb-binlog/pkg/security"
 	"github.com/pingcap/tidb/kv"
 	"github.com/pingcap/tidb/types"
