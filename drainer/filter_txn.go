76061b58c5fcbd5a7a292f0eeda9ee070ef2adf1
tsthght <tsthght@163.com>
2020-03-05 15:30:57 +0800

    add comment

--- drainer/filter_txn.go
+++ drainer/filter_txn.go
@@ -5,6 +5,7 @@ import (
 	"github.com/pingcap/tidb-binlog/pkg/loader"
 )
 
+// LoopBack is the interface that for syncer-plugin
 type LoopBack interface {
 	FilterTxn(txn *loader.Txn, info *loopbacksync.LoopBackSync) (bool, error)
 }
--- pkg/loader/extend_txn.go
+++ pkg/loader/extend_txn.go
@@ -4,6 +4,7 @@ import (
 	"github.com/pingcap/tidb-binlog/drainer/loopbacksync"
 )
 
+// LoopBack is the interface that for loader-plugin
 type LoopBack interface {
 	ExtendTxn(tx *Tx, dmls []*DML, info *loopbacksync.LoopBackSync) (*Tx, []*DML)
 }
