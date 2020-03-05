7782196431442ad6e43b0a820e4894d59ab9fe34
tsthght <tsthght@163.com>
2020-03-05 14:36:36 +0800

    add some log

--- pkg/plugin/plugindemo/demo.go
+++ pkg/plugin/plugindemo/demo.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/pingcap/log"
 	"github.com/pingcap/tidb-binlog/drainer/loopbacksync"
 	"github.com/pingcap/tidb-binlog/pkg/loader"
 )
@@ -9,11 +10,13 @@ type PluginDemo struct{}
 
 func (pd PluginDemo) ExtendTxn(tx *loader.Tx, dmls []*loader.DML, info *loopbacksync.LoopBackSync) (*loader.Tx, []*loader.DML) {
 	//do sth
+	log.Info("i am ExtendTxn")
 	return nil, nil
 }
 
 func (pd PluginDemo) FilterTxn(txn *loader.Txn, info *loopbacksync.LoopBackSync) (bool, error) {
 	//do sth
+	log.Info("i am FilterTxn")
 	return true, nil
 }
 
