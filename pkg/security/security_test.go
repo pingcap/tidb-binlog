b3654f472f761f801673526cf793980a676338e6
Jiahao Huang <july2993@gmail.com>
2020-03-10 16:07:58 +0800

    Reload cert/key for every new conn (#927)
    * Reload cert/key for every new conn
    
    support reload cluster/downstream  TLS cert/key.
    CA still can not be hot-reload now
    
    * expand the loop

--- pkg/security/security_test.go
+++ pkg/security/security_test.go
@@ -29,6 +29,46 @@ func TestClient(t *testing.T) {
 	TestingT(t)
 }
 
+// These certs are generated with:
+//
+// ```sh
+// # generate CA keys
+// openssl ecparam -name secp224r1 -genkey -noout -out ca.key
+// openssl req -x509 -new -nodes -key ca.key -days 999999 -out ca.crt -subj '/CN=localhost'
+//
+// # generate SSL keys
+// openssl ecparam -name secp224r1 -genkey -noout -out ssl.key
+// openssl req -new -key ssl.key -out ssl.csr -subj '/CN=localhost'
+// openssl x509 -req -in ssl.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out ssl.crt -days 999999
+// ```
+var testCa = `
+-----BEGIN CERTIFICATE-----
+MIIBBjCBtQIJAMLMVjQw2v1pMAoGCCqGSM49BAMCMBQxEjAQBgNVBAMMCWxvY2Fs
+aG9zdDAgFw0xOTA0MTcxODEyNDNaGA80NzU3MDMxMzE4MTI0M1owFDESMBAGA1UE
+AwwJbG9jYWxob3N0ME4wEAYHKoZIzj0CAQYFK4EEACEDOgAELwEHdmAcDtBYK9BH
+72q0dKbBBqIG7MZ5+qc+LTcz0OSdhuWkWUZkNN6MqKAPuP7nSo1+21Vb8YswCgYI
+KoZIzj0EAwIDQAAwPQIcbNvV16rOOzwotH65cJY6cCdf0h3IODjlWMf1qAIdAIBB
+Fma6g8iW5zdQPqDR9BGqugNPjtI/SMK6tfQ=
+-----END CERTIFICATE-----
+`
+var testCert = `
+-----BEGIN CERTIFICATE-----
+MIIBBTCBtAIJAP8wfS+6tJ3LMAkGByqGSM49BAEwFDESMBAGA1UEAwwJbG9jYWxo
+b3N0MCAXDTE5MDQxNzE4MTI0NFoYDzQ3NTcwMzEzMTgxMjQ0WjAUMRIwEAYDVQQD
+DAlsb2NhbGhvc3QwTjAQBgcqhkjOPQIBBgUrgQQAIQM6AAQJaXEnDhG2tPxD4wl1
+ycaZwqWm9JeQZFuUPgxekGwCMM22sKpYLvhdKroSBoKWwXIC6vZMWeIj/zAJBgcq
+hkjOPQQBA0EAMD4CHQC05dXi9zFLjYjQGhpJNx+Nc/5vC6E7j/MU+xsTAh0A6SUn
+g916djuFWv8djdDq+0NEFD9OzgPdSb8rZw==
+-----END CERTIFICATE-----
+`
+var testKey = `
+-----BEGIN EC PRIVATE KEY-----
+MGgCAQEEHCsPBVueZ3YX3yp1tn15YXj0cTKGCo1SO1EWO92gBwYFK4EEACGhPAM6
+AAQJaXEnDhG2tPxD4wl1ycaZwqWm9JeQZFuUPgxekGwCMM22sKpYLvhdKroSBoKW
+wXIC6vZMWeIj/w==
+-----END EC PRIVATE KEY-----
+`
+
 var _ = Suite(&testSecuritySuite{})
 
 type testSecuritySuite struct{}
@@ -62,60 +102,32 @@ func (s *testSecuritySuite) TestToTLSConfig(c *C) {
 		SSLKey:  filepath.Join(temp, "ssl.key"),
 	}
 
-	// These certs are generated with:
-	//
-	// ```sh
-	// # generate CA keys
-	// openssl ecparam -name secp224r1 -genkey -noout -out ca.key
-	// openssl req -x509 -new -nodes -key ca.key -days 999999 -out ca.crt -subj '/CN=localhost'
-	//
-	// # generate SSL keys
-	// openssl ecparam -name secp224r1 -genkey -noout -out ssl.key
-	// openssl req -new -key ssl.key -out ssl.csr -subj '/CN=localhost'
-	// openssl x509 -req -in ssl.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out ssl.crt -days 999999
-	// ```
-
-	err := ioutil.WriteFile(dummyConfig.SSLCA, []byte(`
------BEGIN CERTIFICATE-----
-MIIBBjCBtQIJAMLMVjQw2v1pMAoGCCqGSM49BAMCMBQxEjAQBgNVBAMMCWxvY2Fs
-aG9zdDAgFw0xOTA0MTcxODEyNDNaGA80NzU3MDMxMzE4MTI0M1owFDESMBAGA1UE
-AwwJbG9jYWxob3N0ME4wEAYHKoZIzj0CAQYFK4EEACEDOgAELwEHdmAcDtBYK9BH
-72q0dKbBBqIG7MZ5+qc+LTcz0OSdhuWkWUZkNN6MqKAPuP7nSo1+21Vb8YswCgYI
-KoZIzj0EAwIDQAAwPQIcbNvV16rOOzwotH65cJY6cCdf0h3IODjlWMf1qAIdAIBB
-Fma6g8iW5zdQPqDR9BGqugNPjtI/SMK6tfQ=
------END CERTIFICATE-----
-	`), 0644)
+	err := ioutil.WriteFile(dummyConfig.SSLCA, []byte(testCa), 0644)
 	c.Assert(err, IsNil)
 
-	err = ioutil.WriteFile(dummyConfig.SSLCert, []byte(`
------BEGIN CERTIFICATE-----
-MIIBBTCBtAIJAP8wfS+6tJ3LMAkGByqGSM49BAEwFDESMBAGA1UEAwwJbG9jYWxo
-b3N0MCAXDTE5MDQxNzE4MTI0NFoYDzQ3NTcwMzEzMTgxMjQ0WjAUMRIwEAYDVQQD
-DAlsb2NhbGhvc3QwTjAQBgcqhkjOPQIBBgUrgQQAIQM6AAQJaXEnDhG2tPxD4wl1
-ycaZwqWm9JeQZFuUPgxekGwCMM22sKpYLvhdKroSBoKWwXIC6vZMWeIj/zAJBgcq
-hkjOPQQBA0EAMD4CHQC05dXi9zFLjYjQGhpJNx+Nc/5vC6E7j/MU+xsTAh0A6SUn
-g916djuFWv8djdDq+0NEFD9OzgPdSb8rZw==
------END CERTIFICATE-----
-	`), 0644)
+	err = ioutil.WriteFile(dummyConfig.SSLCert, []byte(testCert), 0644)
 	c.Assert(err, IsNil)
 
-	err = ioutil.WriteFile(dummyConfig.SSLKey, []byte(`
------BEGIN EC PRIVATE KEY-----
-MGgCAQEEHCsPBVueZ3YX3yp1tn15YXj0cTKGCo1SO1EWO92gBwYFK4EEACGhPAM6
-AAQJaXEnDhG2tPxD4wl1ycaZwqWm9JeQZFuUPgxekGwCMM22sKpYLvhdKroSBoKW
-wXIC6vZMWeIj/w==
------END EC PRIVATE KEY-----
-	`), 0600)
+	err = ioutil.WriteFile(dummyConfig.SSLKey, []byte(testKey), 0600)
 	c.Assert(err, IsNil)
 
 	config, err := dummyConfig.ToTLSConfig()
 	c.Assert(err, IsNil)
 	c.Assert(config, NotNil)
 	c.Assert(config.RootCAs.Subjects(), HasLen, 1)
-	c.Assert(config.Certificates, HasLen, 1)
-	sslKey, ok := config.Certificates[0].PrivateKey.(*ecdsa.PrivateKey)
+
+	cert, err := config.GetCertificate(nil)
+	c.Assert(err, IsNil)
+	sslKey, ok := cert.PrivateKey.(*ecdsa.PrivateKey)
 	c.Assert(ok, IsTrue)
 	c.Assert(sslKey.Curve, Equals, elliptic.P224())
+
+	cert, err = config.GetClientCertificate(nil)
+	c.Assert(err, IsNil)
+	sslKey, ok = cert.PrivateKey.(*ecdsa.PrivateKey)
+	c.Assert(ok, IsTrue)
+	c.Assert(sslKey.Curve, Equals, elliptic.P224())
+
 }
 
 func (s *testSecuritySuite) TestEmptyTLSConfig(c *C) {
@@ -148,7 +160,17 @@ func (s *testSecuritySuite) TestInvalidTLSConfig(c *C) {
 	err = ioutil.WriteFile(dummyConfig.SSLKey, []byte("invalid key"), 0600)
 	c.Assert(err, IsNil)
 
+	// make ca valid.
+	err = ioutil.WriteFile(dummyConfig.SSLCA, []byte(testCa), 0644)
+	c.Assert(err, IsNil)
 	_, err = dummyConfig.ToTLSConfig()
 	c.Assert(err, ErrorMatches, "could not load client key pair.*")
 
+	// make cert/key valid can check again.
+	err = ioutil.WriteFile(dummyConfig.SSLCert, []byte(testCert), 0644)
+	c.Assert(err, IsNil)
+	err = ioutil.WriteFile(dummyConfig.SSLKey, []byte(testKey), 0600)
+	c.Assert(err, IsNil)
+	_, err = dummyConfig.ToTLSConfig()
+	c.Assert(err, IsNil)
 }
