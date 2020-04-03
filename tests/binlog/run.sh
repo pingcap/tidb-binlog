686819bdac7392eac8588528a7636ca397a3c251
WangXiangUSTC <xiang13225080@163.com>
2020-04-03 22:54:21 +0800

    minor update

--- tests/binlog/run.sh
+++ tests/binlog/run.sh
@@ -9,7 +9,7 @@ run_drainer &
 GO111MODULE=on go build -o out
 
 echo "execute out"
-./out -config ./config.toml > ${OUT_DIR-/tmp}/$TEST_NAME.out 2>&1 || cat ${OUT_DIR-/tmp}/$TEST_NAME.out && exit 1
+./out -config ./config.toml
 
 echo "execute out end"
 killall drainer
