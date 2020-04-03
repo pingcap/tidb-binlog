468f590ca6d84184bba1ce3e7dce7b2dc2c92ccd
WangXiangUSTC <xiang13225080@163.com>
2020-04-03 22:26:25 +0800

    add log

--- tests/binlog/run.sh
+++ tests/binlog/run.sh
@@ -8,6 +8,8 @@ run_drainer &
 
 GO111MODULE=on go build -o out
 
-./out -config ./config.toml > ${OUT_DIR-/tmp}/$TEST_NAME.out 2>&1
+echo "execute out"
+./out -config ./config.toml > ${OUT_DIR-/tmp}/$TEST_NAME.out 2>&1 || cat ${OUT_DIR-/tmp}/$TEST_NAME.out && exit 1
 
+echo "execute out end"
 killall drainer
