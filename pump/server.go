b6be90d3913abbd3af10476676f9362e30b046b5
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2021-10-12 16:17:27 +0800

    drainer: fix filter skip checkpoint problem (#1069) (#1075)

--- pump/server.go
+++ pump/server.go
@@ -469,13 +469,7 @@ func (s *Server) genFakeBinlog() (*pb.Binlog, error) {
 		return nil, errors.Trace(err)
 	}
 
-	bl := &binlog.Binlog{
-		StartTs:  ts,
-		Tp:       binlog.BinlogType_Rollback,
-		CommitTs: ts,
-	}
-
-	return bl, nil
+	return util.GenFakeBinlog(ts), nil
 }
 
 func (s *Server) writeFakeBinlog() (*pb.Binlog, error) {
