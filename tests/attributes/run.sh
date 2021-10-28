8f5e16656890168482e64230047e3aecd95d122d
Ryan Leung <rleungx@gmail.com>
2021-10-28 10:50:20 +0800

    add socket parameter for tidb
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- tests/attributes/run.sh
+++ tests/attributes/run.sh
@@ -9,12 +9,12 @@ run_drainer &
 sleep 3
 
 run_sql "CREATE DATABASE attr;"
-run_sql "CREATE TABLE attr.attributes_t1 (id int primary key, name varchar(50));"
+run_sql "CREATE TABLE attr.attributes_t1 (id INT PRIMARY KEY, name VARCHAR(50));"
 
 run_sql "ALTER TABLE attr.attributes_t1 ATTRIBUTES='merge_option=deny';"
 run_sql "INSERT INTO attr.attributes_t1 (id, name) VALUES (1, 'test1');"
 
-run_sql "CREATE TABLE attr.attributes_t2 (id int primary key, name varchar(50)) PARTITION BY RANGE (id) (PARTITION p0 VALUES LESS THAN (10000), PARTITION p1 VALUES LESS THAN (MAXVALUE));"
+run_sql "CREATE TABLE attr.attributes_t2 (id INT PRIMARY KEY, name VARCHAR(50)) PARTITION BY RANGE (id) (PARTITION p0 VALUES LESS THAN (10000), PARTITION p1 VALUES LESS THAN (MAXVALUE));"
 run_sql "ALTER TABLE attr.attributes_t2 ATTRIBUTES='merge_option=deny';"
 run_sql "ALTER TABLE attr.attributes_t2 PARTITION p0 ATTRIBUTES='merge_option=allow';"
 run_sql "INSERT INTO attr.attributes_t2 (id, name) VALUES (2, 'test2');"
@@ -22,8 +22,8 @@ run_sql "INSERT INTO attr.attributes_t2 (id, name) VALUES (2, 'test2');"
 run_sql "DROP TABLE attr.attributes_t1;"
 run_sql "RECOVER TABLE attr.attributes_t1;"
 run_sql "TRUNCATE TABLE attr.attributes_t1;"
-run_sql "FLASHBACK TABLE attr.attributes_t1 to attr.attributes_t1_back;"
-run_sql "RENAME TABLE attr.attributes_t1_back to attr.attributes_t1_new;"
+run_sql "FLASHBACK TABLE attr.attributes_t1 TO attributes_t1_back;"
+run_sql "RENAME TABLE attr.attributes_t1_back TO attr.attributes_t1_new;"
 run_sql "ALTER TABLE attr.attributes_t2 DROP PARTITION p0;"
 run_sql "ALTER TABLE attr.attributes_t2 TRUNCATE PARTITION p1;"
 
