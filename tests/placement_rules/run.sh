ca802a553e101542d98f07406dd52edc49154c62
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2022-01-12 19:21:09 +0800

    fix placement policy compatibility

---
+++ tests/placement_rules/run.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+cd "$(dirname "$0")"
+
+run_drainer &
+
+sleep 3
+
+run_sql 'CREATE DATABASE placement_test;'
+run_sql 'CREATE PLACEMENT POLICY x1 FOLLOWERS=4'
+run_sql 'CREATE TABLE placement_region.t1 (id INT NOT NULL PRIMARY KEY, b VARCHAR(100)) PLACEMENT POLICY=x1;'
+run_sql "INSERT INTO placement_test.t1 (a,b) VALUES(1,'a'),(2,'b'),(3,'c'),(4,'d');"
+run_sql "INSERT INTO placement_test.t1 (a,b) VALUES(5,'e'),(6,'f'),(7,'g'),(8,'h');"
+
+sleep 3
+
+down_run_sql 'SELECT a, b FROM cache_test.t1 order by a'
+check_contains 'a: 7'
+check_contains 'b: g'
+check_contains 'a: 8'
+check_contains 'b: h'
+
+run_sql 'DROP DATABASE placement_test'
+
+killall drainer
