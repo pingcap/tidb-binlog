e7a3fde98300188f5ceba37f8c487c1b1e0f327b
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2022-01-13 16:59:43 +0800

    drainer: fix placement policy compatibility (#1119) (#1120)
    close pingcap/tidb-binlog#1118, close pingcap/tidb#31611

---
+++ tests/placement_rules/run.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+cd "$(dirname "$0")"
+
+run_drainer &
+
+sleep 3
+
+run_sql 'CREATE DATABASE placement_test;'
+run_sql 'SET @@global.tidb_enable_alter_placement=1'
+run_sql 'CREATE PLACEMENT POLICY x1 FOLLOWERS=4'
+run_sql 'CREATE TABLE placement_test.t1 (a INT NOT NULL PRIMARY KEY, b VARCHAR(100)) PLACEMENT POLICY=x1;'
+run_sql "INSERT INTO placement_test.t1 (a,b) VALUES(1,'a'),(2,'b'),(3,'c'),(4,'d');"
+run_sql "INSERT INTO placement_test.t1 (a,b) VALUES(5,'e'),(6,'f'),(7,'g'),(8,'h');"
+down_run_sql 'SET @@global.tidb_enable_alter_placement=1'
+down_run_sql 'CREATE PLACEMENT POLICY x1 FOLLOWERS=4'
+
+sleep 3
+
+down_run_sql 'SELECT a, b FROM placement_test.t1 order by a'
+check_contains 'a: 7'
+check_contains 'b: g'
+check_contains 'a: 8'
+check_contains 'b: h'
+
+run_sql 'DROP DATABASE placement_test'
+
+killall drainer
