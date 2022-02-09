cd6606957ef3a620c302375dd12bb00efa492d33
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2022-02-09 15:13:36 +0800

    reparo: fix can't decode bit type data (#1103) (#1106)

--- tests/dailytest/db.go
+++ tests/dailytest/db.go
@@ -221,6 +221,11 @@ func genColumnData(table *table, column *column) (string, error) {
 	isUnsigned := mysql.HasUnsignedFlag(tp.Flag)
 
 	switch tp.Tp {
+	case mysql.TypeBit:
+		if randInt(0, 1) == 0 {
+			return "b'0'", nil
+		}
+		return "b'1'", nil
 	case mysql.TypeTiny:
 		var data int64
 		if isUnique {
