fc14c4a4555accafa15c625be4273122d9f52267
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2022-03-29 10:40:29 +0800

    pump: try to fix marshal marshaling: proto: Marshal called with nil error (#1153) (#1155)
    close pingcap/tidb-binlog#1154

--- tests/restart/run.sh
+++ tests/restart/run.sh
@@ -24,8 +24,11 @@ sleep 5
 sleep 5
 
 # restart pumps
-run_pump 8250 &
 run_pump 8251 &
+# make sure there is always one pump alive
+sleep 2
+check_status pumps "pump:8251" online
+run_pump 8250 &
 
 sleep 5
 
